453b6526b038a36645f27df10a24eb2d05f7868b
Ri <rmerritt6@student.gsu.edu>
2024-10-08 19:29:58 -0400

    Added music and changed the background

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:just_audio/just_audio.dart';
 
 void main() {
   runApp(TheScariestGameEver());
@@ -24,11 +25,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   double _skullPositionX = 50;
   double _skullPositionY = 50;
+  final player = AudioPlayer();  // Audio player instance
 
   @override
   void initState() {
     super.initState();
     _startMovingSkull();
+    _playBackgroundMusic();  // Start playing background music
   }
 
   void _startMovingSkull() {
@@ -40,6 +43,11 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void _playBackgroundMusic() async {
+    await player.setAsset('assets/images/[Free] _Spooky_ _ Halloween Drill Type Beat [ ezmp3.cc ].mp3');  // audio
+    player.play();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,36 +56,34 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Stack(
         children: [
-          
+          // Background image
           Positioned.fill(
             child: Image.asset(
-              'assets/images/ghosts.png',  // Background image
+              'assets/images/creepy background.jpg',  // Background image
               fit: BoxFit.cover,
             ),
           ),
-          
+          // Animated Creepy Skull
           AnimatedPositioned(
             duration: Duration(seconds: 1),
             left: _skullPositionX,
             top: _skullPositionY,
             child: GestureDetector(
               onTap: () {
-                // Action when skull is clicked
                 _showSpookyReaction('Creepy Skull');
               },
               child: Image.asset(
-                'assets/images/creepy skull.jpg',
+                'assets/images/creepy skull.jpg',  // Skull image
                 width: 100,
               ),
             ),
           ),
-          
+          // Pumpkin Image
           Positioned(
             bottom: 20,
             right: 20,
             child: GestureDetector(
               onTap: () {
-                // Action when pumpkin is clicked
                 _showSpookyReaction('Pumpkin');
               },
               child: Image.asset(
